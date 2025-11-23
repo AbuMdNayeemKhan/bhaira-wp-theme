40ce5e3e1ec89c2c3e6948a0fbeaf482ffd83ac6
AbuMdNayeemKhan <abumdnayeemkhan@gmail.com>
2025-11-23 21:00:36 +0600

    Start building word press theme

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+/*
+* My Theme Function
+*/
+
+
+// Theme Title
+add_theme_support('title-tag');
+
+// Theme CSS an jQuery file calling
+function bhaira_css_js_file_calling() {
+    // CSS File calling
+    
+}
+
+
+
+?>
