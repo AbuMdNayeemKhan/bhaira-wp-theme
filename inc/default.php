56c1e60c101d34c7139e01082f65ecf1fbc1d212
AbuMdNayeemKhan <abumdnayeemkhan@gmail.com>
2026-01-30 19:57:47 +0600

    the_excerpt added

--- inc/default.php
+++ inc/default.php
@@ -6,4 +6,15 @@ add_theme_support('title-tag');
 add_theme_support( 'post-thumbnails', array('page', 'post'));
 add_image_size('post-thumbnails', 970, 350, true);
 
+// Except read more button add
+function bhaira_excerpt_more($more){
+    return '<a class="excerpt-readmore-btn" href="'. get_permalink( get_the_ID(  ) ) .'"> Read More </a>';
+};
+add_filter( 'excerpt_more', 'bhaira_excerpt_more' );
+
+// Except lenth Control
+function bhaira_excerpt_length($length){
+    return 36;
+}
+add_filter( 'excerpt_length', 'bhaira_excerpt_length', 999 );
 ?>
